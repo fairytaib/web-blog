f8be28f78176ee1b4c79e00d0616d4816b43fb87
fairytaib <fairytaib@gmail.com>
2025-03-01 13:53:14 +0100

    commit

--- blog/admin.py
+++ blog/admin.py
@@ -7,9 +7,9 @@ from .models import Post, Comment
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
-    list_display = ('title', 'slug', 'status')
+    list_display = ('title', 'status', "created_on")
     search_fields = ['title']
-    list_filter = ('status',)
+    list_filter = ('status', "created_on")
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
 
