954587ee5b102040351d29d529a082fa1d2222a7
fairytaib <fairytaib@gmail.com>
2025-03-17 09:32:46 +0100

    Commit

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.PostList.as_view(), name="home"),
+    path("profile/", views.profile_page, name="profile"),
     path("<slug:slug>/", views.post_detail, name="post_detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
-
+from .forms import CommentForm
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
@@ -25,12 +27,36 @@ def post_detail(request, slug):
 
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
+    comments = post.comments.all().order_by("-created_on")
+    comments_count = post.comments.filter(approved=True).count()
+    comment_form = CommentForm()
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+    if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        comment.author = request.user
+        comment.post = post
+        comment.save()
+        messages.add_message(
+        request, messages.SUCCESS,
+        'Comment submitted and awaiting approval'
+        )
 
     context = {"post": post,
-               "code": "Sophjan"}
+               "comments": comments,
+               "comments_count": comments_count,
+               "comment_form": comment_form}
 
     return render(
         request,
         "blog/post_detail.html",
         context
     )
+
+
+def profile_page(request):
+    user = get_object_or_404(User, username=request.user.username)
+    comments = user.commenter.all()
+    context = {"user": user, "comments": comments}
+    return render(request, "blog/profile.html", context)
